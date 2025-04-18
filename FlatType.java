53e13908ccb0dade0cc139391ac7ee10c42f7387
sohha <sohhaoming@gmail.com>
2025-04-19 02:00:13 +0800

    updated flattype

--- FlatType.java
+++ FlatType.java
@@ -1,26 +1,47 @@
-public class FlatType{
+public class FlatType {
     private String typeName;
     private int basePrice;
     private int availableUnits;
-
-    public FlatType(String typeName, int basePrice, int availableUnits) {
+    
+    public FlatType(String typeName, int availableUnits, double basePrice) {
         this.typeName = typeName;
-        this.basePrice = basePrice;
         this.availableUnits = availableUnits;
+        this.basePrice = (int) basePrice;
     }
+    
     public boolean isAvail() {
         return availableUnits > 0;
     }
-
     
     public void decrementUnit() {
         if (availableUnits > 0) {
             availableUnits--;
         }
     }
-
     
     public void incrementUnit() {
         availableUnits++;
+    }
+    
+    public int getRemainingUnits() {
+        return availableUnits;
+    }
+    
+    public String getName() {
+        return typeName;
+    }
+    
+    public double getPrice() {
+        return basePrice;
+    }
+    
+    public int getUnitCount() {
+        return availableUnits;
+    }
+    
+    public void setUnitCount(int count) {
+        if (count >= 0) {
+            this.availableUnits = count;
+        }
     }
 }
